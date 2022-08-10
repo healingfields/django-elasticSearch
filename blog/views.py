9781d0f90b3a9bcb47c7147eb51bacb9444b4a9d
healingfields <dihratene@gmail.com>
2022-08-10 20:59:01 +0100

    created elastic documents

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .models import Category, Article
+from blog.serializers import CategorySerializer, ArticleSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
